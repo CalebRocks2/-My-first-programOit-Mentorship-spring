ecf54c7043727ae7e5e056f6a110a8833c21cee7
CalebRocks2 <131414452+CalebRocks2@users.noreply.github.com>
2023-04-20 20:04:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# -My-first-programOit-Mentorship-spring
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qdf3dn)
---
+++ index.js
@@ -0,0 +1,23 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+console.log("Hello world!");
+
+function myFunction(input) {
+console.log(input)
+}
+
+myFunction("Out in Tech");
+
+// This is a comment.
+
+function variables() {
+  var foo ="bar";
+
+myFunction(foo)
+}
+
+variables();
